1f96db6a932c271b48b21bc2b3b724f4b79023e7
yoshiodei <yoshiodei@yahoo.com>
2021-10-31 12:12:52 +0000

    change Home component name to Fixtures

---
+++ src/components/Fixtures.jsx
@@ -0,0 +1,13 @@
+const Fixtures = () => {
+    return (
+        <section className="fixtures">
+            <h1>Fixtures</h1>
+            
+
+        </section>
+    )
+}
+
+export default Fixtures;
+
+
