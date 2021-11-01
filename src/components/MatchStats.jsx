35a1bdafad7941c779b19033e0f5269794e71948
yoshiodei <yoshiodei@yahoo.com>
2021-11-01 09:58:46 +0000

    add component MatchStat

---
+++ src/components/MatchStats.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Match from '../Match';
+import './../../public/css/match__stats.css';
+
+const Matchstats = () => {
+    return (
+        <section className="pages">
+            <Match />
+            <div className="match__stats">
+                hi
+            </div>
+        </section>
+    );
+}
+
+export default Matchstats;
